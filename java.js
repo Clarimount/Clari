654e276fa80d3033785487724efe3596b2afaecd
Clari <52334273+Clarimount@users.noreply.github.com>
2019-12-13 16:17:49 -0800

    changed alot so yea

---
+++ java.js
@@ -0,0 +1,10 @@
+window.onscroll = function () { scrollfunc() };
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function scrollfunc() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
