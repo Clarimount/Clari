23914ef84d56b7aae99524f0474fb4f30f6e5373
Clari <52334273+Clarimount@users.noreply.github.com>
2019-12-20 13:33:27 -0800

    Update java.js

--- java.js
+++ java.js
@@ -8,3 +8,17 @@ function scrollfunc() {
         navbar.classList.remove("sticky");
     }
 }
+
+window.onclick = function () { chartab() };
+var char-tab = document.getElementById("char-tab");
+var char-body = document.getElementById("char-body");
+function chartab() {
+    if(window.Onclick = true) {
+        char-tab.classList.add("flex")
+        char-body.classList.add("flex")
+    } else {
+     char-tab.classList.remove("flex")
+     char-body.classList.remove("flex")
+    }
+    
+}
