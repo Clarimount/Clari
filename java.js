8116eed032c6b2771e22712f63aa0e3deb06ea34
Clari <52334273+Clarimount@users.noreply.github.com>
2019-12-24 12:20:47 -0800

    character tab links added

--- java.js
+++ java.js
@@ -9,16 +9,4 @@ function scrollfunc() {
     }
 }
 
-window.onclick = function () { chartab() };
-var char-tab = document.getElementById("char-tab");
-var char-body = document.getElementById("char-body");
-function chartab() {
-    if(window.Onclick = true) {
-        char-tab.classList.add("flex")
-        char-body.classList.add("flex")
-    } else {
-     char-tab.classList.remove("flex")
-     char-body.classList.remove("flex")
-    }
-    
-}
+
