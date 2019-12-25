5d3b1885c6d6df249fe92aad2242d03b12219e3e
Clari <52334273+Clarimount@users.noreply.github.com>
2019-12-25 10:47:12 -0800

    changed a bunch

--- java.js
+++ java.js
@@ -1,3 +1,4 @@
+/*
 window.onscroll = function () { scrollfunc() };
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
@@ -8,5 +9,22 @@ function scrollfunc() {
         navbar.classList.remove("sticky");
     }
 }
+*/
+function openCharacter(evt, CharName) {
+  var i, charactercontent, tablinks;
+  charactercontent = document.getElementsByClassName("charactercontent");
+  for (i = 0; i < charactercontent.length; i++) {
+    charactercontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(CharName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
-
+function Hewwo()
+{
+    document.getElementById('demo').innerHTML = Date()
+}
